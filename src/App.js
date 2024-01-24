d65bede59517803b06c6c7a772318da34edf78c1
Katerina <ekaterina.tereshko.me@gmail.com>
2024-01-24 23:05:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import dollar from "./dollar.svg"
+import Cat from './Cat';
 
 function App() {
+
+  const [myAdvice, setMyAdvice] = useState("");
+  const [myPrice, setMyPrice] = useState("");
+
+    const getAdvice = async () => {
+      const response = await fetch("https://www.boredapi.com/api/activity");
+      const data = await response.json();
+      setMyAdvice(data.activity);
+      setMyPrice(data.price);
+    }
+
+  useEffect( () => {
+    getAdvice()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="main">
+
+        <div className="container bigger-margin">
+          <h1>Bored? Don't be!</h1>
+          <h2>Today you can...</h2>
+          <p className="advice">{myAdvice}</p>
+          <img src={dollar} width="50px" alt="dollar-coin"/>
+          <p className="price">It will cost you... {(myPrice === 0) ? "nothing!" : `${myPrice*50} dollars!`}</p>
+        </div>
+
+        <div className='container'>
+          <button onClick={getAdvice}>New Tip</button>
+        </div>
+
+      </div>
+
+      <Cat/>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Cat.js
@@ -0,0 +1,13 @@
+import cat1 from "./Cat1.svg"
+
+function Cat(){
+
+  return (
+    <div className="container">
+      <img width="300px" alt="cat" src={cat1}/>
+    </div>
+  );
+
+}
+
+export default Cat;
